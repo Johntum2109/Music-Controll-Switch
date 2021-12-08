d6c5b36f98bdee638847662f10515533fbf3eaa8
Johntum2109 <78987759+Johntum2109@users.noreply.github.com>
2021-12-08 18:58:32 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function preload() {}
+// Songs
+var song1 = "";
+var song2 = ""; 
+
+function preload() 
+{
+    song1 = loadSound("music1.mp3");
+    song2 = loadSound("music2.mp3");
+}
 
 function setup() 
 {
